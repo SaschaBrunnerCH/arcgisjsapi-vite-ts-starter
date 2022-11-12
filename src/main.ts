4be6e9fd2d754c3bb633497e6f0ec363a2870450
Sascha Brunner <sbrunner@esri.com>
2022-11-12 08:32:26 +0100

    add some useful code snippets

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import './style.css'
 import SceneView from "@arcgis/core/views/SceneView";
 import Map from "@arcgis/core/Map";
+import * as reactiveUtils from "@arcgis/core/core/reactiveUtils";
 
-new SceneView({
+const view = new SceneView({
     container: "viewDiv",
     map: new Map({
         basemap: "topo-vector",
         ground: "world-elevation"
     })
+});
+
+// Store view on the window so we can access for debugging purposes
+(window as any).view = view;
+
+view.when(async () => {
+    await reactiveUtils.whenOnce(() => !view.updating);
+    console.log("view loaded and updated")
 });
